795993c5b56a4fcc416e02055ec0e53d62ea64e3
Alperenakkal <alperenakkal06@gmail.com>
2023-11-04 19:04:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Window Object
+let value;
+value=document;
+value=document.all;
+value=document.all.length;
+value=document.all[0];
+value=document.all[6];
+const elements=document.all;
+// elements.forEach(function(element){
+    
+ 
+//     console.log(element);
+// });
+// const collections=Array.from(document.all);
+// collections.forEach(function(collections){
+//     console.log(collections)
+// })
+
+// console.log(value);
